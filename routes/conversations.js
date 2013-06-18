02045b250fe86d1036318f7ad69be36b462f662e
goofyahead <goofyahead@goofyahead-VirtualBox.(none)>
2013-06-18 15:48:46 +0200

    check msg id, interval check mail stopped

--- routes/conversations.js
+++ routes/conversations.js
@@ -10,12 +10,12 @@ var async = require('async');
 
 
 function show(obj) {
-  return inspect(obj, false, Infinity);
+	return inspect(obj, false, Infinity);
 }
 
 function die(err) {
-  console.log('Uh oh: ' + err);
-  process.exit(1);
+	console.log('Uh oh: ' + err);
+	process.exit(1);
 }
 
 exports.retrieveConversations = function (req, res){
@@ -28,197 +28,198 @@ exports.retrieveConversations = function (req, res){
 	var config = configs.getConfig(credentials['user']);
 
 	var imap = new Imap({
-      user: credentials.user,
-      password: credentials.pass,
-      host: config.host,
-      port: config.port,
-      secure: config.secure
-    });
+		user: credentials.user,
+		password: credentials.pass,
+		host: config.host,
+		port: config.port,
+		secure: config.secure
+	});
 
-    var messagesList = {};
+	var messagesList = {};
 	var conversations = []; // {title: "check this out", messages: [ "asdfasdf", "asdfasdfsdf"]}
 
-    configs.getBoxes(imap, credentials['user'], config, function () {
-    
-    var size = config.box.length;
+	configs.getBoxes(imap, credentials['user'], config, function () {
 
-	imap.connect(function(err) {
-	    if (err) die(err);
-	    async.eachSeries( config.box, function (box, cb) {
-	    	imap.openBox(box, true, function (err, mailbox) {
-	    		console.log('opening ');
-				console.log(mailbox);
+		var size = config.box.length;
 
-				if (mailbox.messages.total == 0){
-					console.log('should skip'.red);
-					cb(null);
-					return;
-				} 
-				if (err) die(err);
-				imap.search([ 'ALL', ['SINCE', 'June 1, 2012'] ], function(err, results) {
+		imap.connect(function(err) {
+			if (err) die(err);
+			async.eachSeries( config.box, function (box, cb) {
+				imap.openBox(box, true, function (err, mailbox) {
+					console.log('opening ');
+					console.log(mailbox);
+
+					if (mailbox.messages.total == 0){
+						console.log('should skip'.red);
+						cb(null);
+						return;
+					} 
+					if (err) die(err);
+					imap.search([ 'ALL', ['SINCE', 'June 1, 2012'] ], function(err, results) {
 					 // console.log('status ///////////////////////////'.green);
 					 // console.log(messagesList);
 
-					if (err) die(err);
-					imap.fetch(results, { headers: true,
-						body: false,
-						cb: function(fetch) {
-						fetch.on('message', function(msg) {
+					 if (err) die(err);
+					 imap.fetch(results, { 
+					 	headers: true,
+					 	body: false,
+					 	cb: function(fetch) {
+					 		fetch.on('message', function(msg) {
 
-							msg.on('headers', function(hdrs) {
+					 			msg.on('headers', function(hdrs) {
 								// console.log(hdrs);
 								if (hdrs['message-id']) {
-								if ( ! messagesList[util.normalizeMessageId(hdrs['message-id'].toString())]) {
-				                var related = null;
-				                var references = [];
-				                // add in reply to to the references.
-				                if (hdrs['in-reply-to']) {
-				                  references.push(util.normalizeMessageId(hdrs['in-reply-to'][0].toString()));
-				                }
+									if ( ! messagesList[util.normalizeMessageId(hdrs['message-id'].toString())]) {
+										var related = null;
+										var references = [];
+						                // add in reply to to the references.
+						                if (hdrs['in-reply-to']) {
+						                	references.push(util.normalizeMessageId(hdrs['in-reply-to'][0].toString()));
+						                }
+
+						                //add references
+						                if (hdrs['references'] && util.parseReferences(hdrs['references'][0])) {
+						                	util.parseReferences(hdrs['references'][0]).forEach(function (elem){
+						                		references.push(elem);
+						                	});
+						                }
+
+						                //search for existing containers
+						                references.forEach(function (uid){
+						                	if (messagesList[uid]) {
+						                      related = messagesList[uid]; //there is a container related
+						                      return;
+						                  }
+						              });
+
+						                //container exists, fill the list and container with relations and myself
+						                if (related != null) {
+						                  // if doesnt have tittle set it.
+						                  if (related.title == '' && hdrs['subject']) related.title = util.normalizeSubject(hdrs['subject'].toString());
+
+						                  //get related uids
+						                  var relatedUID = [];
+						                  related.messages.forEach(function (messageObj) {
+						                  	relatedUID.push(messageObj.messageId);
+						                  		//console.log('storing'.yellow + messageObj.messageId);
+						                  	});
+
+										  //iterate references and link them to this container
+										  references.forEach(function (uid) {
+						                  	//console.log(uid.toString().green);
+						                  	if (! _.contains(relatedUID, uid)){
+						                    	related.messages.push({ path: box, messageId: uid}); // add refs to the container
+						                    	relatedUID.push(uid);
+						                    	//console.log('adding '.red + uid);
+						                    } 
+						                    if (! messagesList[uid]) messagesList[uid] = related;
+						                });
+
+						                  //add this UID to the list and link to container
+						                  related.messages.push({ path: box, messageId: util.normalizeMessageId(hdrs['message-id'].toString())});
+						                  messagesList[util.normalizeMessageId(hdrs['message-id'].toString())] = related;
+
+						                } else { //there is not container for this message or any related message
+						                  //create a new container
+						                  var container = {title : '', messages : []};
+
+						                  //add myself
+						                  container['messages'].push({ path: box, messageId: util.normalizeMessageId(hdrs['message-id'].toString())});
+						                  //set subject
+						                  if (hdrs['subject']) container.title = util.normalizeSubject(hdrs['subject'].toString());
+						                  // link list with container
+						                  messagesList[util.normalizeMessageId(hdrs['message-id'].toString())] = container;
+
+						                   //get related uids
+						                   var relatedUID = [];
+						                   container.messages.forEach(function (messageObj){
+						                   	relatedUID.push(messageObj.messageId);
+							                  		//console.log('adding '.red + messageObj.messageId);
+							                  	});
+
+											  //iterate references and link them to this container
+											  references.forEach(function (uid){
+											  	if (! _.contains(relatedUID, uid)) {
+							                    	container.messages.push({ path: box, messageId: uid}); // add refs to the container
+							                    	relatedUID.push(uid);
+							                    	//console.log('adding '.green + uid);
+							                    }
+							                    if (! messagesList[uid]) messagesList[uid] = container;
+							                });
+
+											  conversations.push(container);
+											}
+									} else {
+					                // do we need to add refs or check them if this message is already defined? maybe for reply-to?
+					                var container = messagesList[util.normalizeMessageId(hdrs['message-id'].toString())];
+					                var references = [];
+					                // add in reply to to the references.
+					                if (hdrs['in-reply-to']) {
+					                	references.push(util.normalizeMessageId(hdrs['in-reply-to'][0].toString()));
+					                }
 
 				                //add references
-				                if (hdrs['references'] && util.parseReferences(hdrs['references'][0])) {
-				                  util.parseReferences(hdrs['references'][0]).forEach(function (elem){
-				                    references.push(elem);
-				                  });
+				                if (hdrs['references']) {
+				                	util.parseReferences(hdrs['references'][0]).forEach(function (elem){
+				                		references.push(elem);
+				                	});
 				                }
 
 				                //search for existing containers
-				                references.forEach(function (uid){
-				                    if (messagesList[uid]) {
-				                      related = messagesList[uid]; //there is a container related
-				                      return;
-				                    }
+				                //get related uids
+				                var relatedUID = [];
+				                container.messages.forEach(function (messageObj){
+				                	relatedUID.push(messageObj.messageId);
 				                });
 
-				                //container exists, fill the list and container with relations and myself
-				                if (related != null) {
-				                  // if doesnt have tittle set it.
-				                  if (related.title == '' && hdrs['subject']) related.title = util.normalizeSubject(hdrs['subject'].toString());
+				                //iterate references and link them to this container
+				                references.forEach(function (uid){
+				                	if (! _.contains(relatedUID, uid)){
+				                  	container.messages.push({ path: box, messageId: uid}); // add refs to the container
+				                  	relatedUID.push(uid);
+				                  }
+				                  if (! messagesList[uid]) messagesList[uid] = container;
+				              });
+				            }
+			        
 
-				                  //get related uids
-				                  var relatedUID = [];
-				                  related.messages.forEach(function (messageObj) {
-				                  		relatedUID.push(messageObj.messageId);
-				                  		//console.log('storing'.yellow + messageObj.messageId);
-				                  });
+					        console.log('Flags: ' + msg.flags);
+					        if ( ! _.contains(msg.flags, '\\Seen')){
+					        	console.log('unread'.red);
+					        	messagesList[util.normalizeMessageId(hdrs['message-id'].toString())]['unread'] = true;
+					        } else {
+					        	if (! messagesList[util.normalizeMessageId(hdrs['message-id'].toString())]['unread'])
+					        		messagesList[util.normalizeMessageId(hdrs['message-id'].toString())]['unread'] = false;
+					        }
 
-								  //iterate references and link them to this container
-				                  references.forEach(function (uid) {
-				                  	//console.log(uid.toString().green);
-				                    if (! _.contains(relatedUID, uid)){
-				                    	related.messages.push({ path: box, messageId: uid}); // add refs to the container
-				                    	relatedUID.push(uid);
-				                    	//console.log('adding '.red + uid);
-				                    } 
-				                    if (! messagesList[uid]) messagesList[uid] = related;
-				                  });
+				            //find the container, find the message, add the UID.
+				            console.log('checking '.green + util.normalizeMessageId(hdrs['message-id'].toString()));
+				            messagesList[util.normalizeMessageId(hdrs['message-id'].toString())].messages.forEach( function (messageObj) {
+				            	if (messageObj.messageId == util.normalizeMessageId(hdrs['message-id'].toString())){
+				            		messageObj['messageUid'] = msg.uid;
+				            	}
+				            });
+				        }
+				}); //headers
 
-				                  //add this UID to the list and link to container
-				                  related.messages.push({ path: box, messageId: util.normalizeMessageId(hdrs['message-id'].toString())});
-				                  messagesList[util.normalizeMessageId(hdrs['message-id'].toString())] = related;
-
-				                } else { //there is not container for this message or any related message
-			                  //create a new container
-			                  var container = {title : '', messages : []};
-
-			                  //add myself
-			                  container['messages'].push({ path: box, messageId: util.normalizeMessageId(hdrs['message-id'].toString())});
-			                  //set subject
-			                  if (hdrs['subject']) container.title = util.normalizeSubject(hdrs['subject'].toString());
-			                  // link list with container
-			                  messagesList[util.normalizeMessageId(hdrs['message-id'].toString())] = container;
-
-			                   //get related uids
-				                  var relatedUID = [];
-				                  container.messages.forEach(function (messageObj){
-				                  		relatedUID.push(messageObj.messageId);
-				                  		//console.log('adding '.red + messageObj.messageId);
-				                  });
-
-								  //iterate references and link them to this container
-				                  references.forEach(function (uid){
-				                    if (! _.contains(relatedUID, uid)) {
-				                    	container.messages.push({ path: box, messageId: uid}); // add refs to the container
-				                    	relatedUID.push(uid);
-				                    	//console.log('adding '.green + uid);
-				                    }
-				                    if (! messagesList[uid]) messagesList[uid] = container;
-				                  });
-
-			                  conversations.push(container);
-			                	}
-			              	} else {
-			                // do we need to add refs or check them if this message is already defined? maybe for reply-to?
-			                var container = messagesList[util.normalizeMessageId(hdrs['message-id'].toString())];
-			                var references = [];
-			                // add in reply to to the references.
-			                if (hdrs['in-reply-to']) {
-			                  references.push(util.normalizeMessageId(hdrs['in-reply-to'][0].toString()));
-			                }
-
-			                //add references
-			                if (hdrs['references']) {
-			                  util.parseReferences(hdrs['references'][0]).forEach(function (elem){
-			                    references.push(elem);
-			                  });
-			                }
-
-			                //search for existing containers
-			                //get related uids
-			                  var relatedUID = [];
-			                  container.messages.forEach(function (messageObj){
-			                  		relatedUID.push(messageObj.messageId);
-			                  });
-				                  
-			                //iterate references and link them to this container
-			                references.forEach(function (uid){
-			                  if (! _.contains(relatedUID, uid)){
-			                  	container.messages.push({ path: box, messageId: uid}); // add refs to the container
-			                  	relatedUID.push(uid);
-			                  }
-			                  if (! messagesList[uid]) messagesList[uid] = container;
-			                });
-			              }
-			          	}
-
-						console.log('Flags: ' + msg.flags);
-			            if ( ! _.contains(msg.flags, '\\Seen')){
-			              	console.log('unread'.red);
-			              	messagesList[util.normalizeMessageId(hdrs['message-id'].toString())]['unread'] = true;
-			            } else {
-			            	if (! messagesList[util.normalizeMessageId(hdrs['message-id'].toString())]['unread'])
-			            	messagesList[util.normalizeMessageId(hdrs['message-id'].toString())]['unread'] = false;
-			            }
-
-			            //find the container, find the message, add the UID.
-			            console.log('checking '.green + util.normalizeMessageId(hdrs['message-id'].toString()));
-			            messagesList[util.normalizeMessageId(hdrs['message-id'].toString())].messages.forEach( function (messageObj) {
-			            	if (messageObj.messageId == util.normalizeMessageId(hdrs['message-id'].toString())){
-			            		messageObj['messageUid'] = msg.uid;
-			            	}
-			            });
-			            
-						}); //headers
-
-			            msg.on('end', function() {
-			              console.log('Finished message no. ' + msg.seqno);
-			              console.log('uid ' + msg.uid);
-			            });
-
-		    			});
-	    		}}, function(err) {
-	    			cb(null);
-	    		});
+			msg.on('end', function() {
+				console.log('Finished message no. ' + msg.seqno);
+				console.log('uid ' + msg.uid);
 			});
-			});
-	    }, function (err) {
-	    	console.log('Done fetching all messages!'.green);
-	        imap.logout();
-	        console.log(conversations);
-	        res.send(conversations);
-	    });
 
-	    });//imap connect
-    });
+			});
+		}}, function(err) {
+			cb(null);
+			});
+		});
+	});
+	}, function (err) {
+		console.log('Done fetching all messages!'.green);
+		imap.logout();
+		console.log(conversations);
+		res.send(conversations);
+	});
+
+		    });//imap connect
+	});
 }
--- routes/newmail.js
+++ routes/newmail.js
@@ -6,58 +6,58 @@ var colors = require('colors');
 var accounts = [{user : 'alex@ark.com', pass: '***************', lastUid: 0}];
 var imap;
 
-setInterval(function () {
-	console.log('checking new email for '.green + accounts[0].user.toString().green);
-	imap = new Imap({
-      user: accounts[0].user,
-      password: accounts[0].pass,
-      host: 'imap.gmail.com',
-      port: 993,
-      secure: true
-    });
+// setInterval(function () {
+// 	console.log('checking new email for '.green + accounts[0].user.toString().green);
+// 	imap = new Imap({
+//       user: accounts[0].user,
+//       password: accounts[0].pass,
+//       host: 'imap.gmail.com',
+//       port: 993,
+//       secure: true
+//     });
 
-	openInbox(function(err, mailbox) {
-		var currentMax = 0;
-		var message = '';
-		var from = '';
-	  if (err) die(err);
-	  imap.search([ 'UNSEEN' ], function(err, results) {
-	    if (err) die(err);
-	    imap.fetch(results,
-	      { headers: ['from', 'to', 'subject', 'date'],
-	        cb: function(fetch) {
-	          fetch.on('message', function(msg) {
+// 	openInbox(function(err, mailbox) {
+// 		var currentMax = 0;
+// 		var message = '';
+// 		var from = '';
+// 	  if (err) die(err);
+// 	  imap.search([ 'UNSEEN' ], function(err, results) {
+// 	    if (err) die(err);
+// 	    imap.fetch(results,
+// 	      { headers: ['from', 'to', 'subject', 'date'],
+// 	        cb: function(fetch) {
+// 	          fetch.on('message', function(msg) {
 	            
-	            msg.on('headers', function(hdrs) {
-	            	if (currentMax < msg.uid){
-	              		currentMax = msg.uid;
-	              		message = hdrs['subject'].toString();
-	              		from = hdrs['from'].toString();
-	              	} 
-	            });
-	            msg.on('end', function() {
-	              console.log('Finished message no. ' + msg.uid);
+// 	            msg.on('headers', function(hdrs) {
+// 	            	if (currentMax < msg.uid){
+// 	              		currentMax = msg.uid;
+// 	              		message = hdrs['subject'].toString();
+// 	              		from = hdrs['from'].toString();
+// 	              	} 
+// 	            });
+// 	            msg.on('end', function() {
+// 	              console.log('Finished message no. ' + msg.uid);
 
-	            });
-	          });
-	        }
-	      }, function(err) {
-	        if (err) throw err;
-	        console.log('Done fetching all messages!');
-	        if (accounts[0].lastUid == 0){
-	        	console.log('setting a new max ID'.green);
-	        	accounts[0].lastUid = currentMax;
-	        }
-	        if (accounts[0].lastUid < currentMax) {
-	        	console.log('NEW MESSAGE!  '.red + message.red + ' from ' + from);
-	        	accounts[0].lastUid = currentMax;
-	        }
-	        imap.logout();
-	      }
-	    );
-	  });
-	});
-}, 30000);
+// 	            });
+// 	          });
+// 	        }
+// 	      }, function(err) {
+// 	        if (err) throw err;
+// 	        console.log('Done fetching all messages!');
+// 	        if (accounts[0].lastUid == 0){
+// 	        	console.log('setting a new max ID'.green);
+// 	        	accounts[0].lastUid = currentMax;
+// 	        }
+// 	        if (accounts[0].lastUid < currentMax) {
+// 	        	console.log('NEW MESSAGE!  '.red + message.red + ' from ' + from);
+// 	        	accounts[0].lastUid = currentMax;
+// 	        }
+// 	        imap.logout();
+// 	      }
+// 	    );
+// 	  });
+// 	});
+// }, 30000);
 
 
 function openInbox(cb) {
